35f623344312ec3d6c86a2fb8624541626c22580
Kevin Conner <kev.conner@getupcloud.com>
2024-04-30 08:29:40 -0700

    Updated to pass through env and mount points

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,31 @@
 #!/usr/bin/env bash
 
-if [ ! -z $INPUT_USERNAME ];
-then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+# Copy all environment variables which start with INPUT_, GITHUB_, RUNNER_ or ACTIONS_
+# Also set HOME and CI
+
+vars=$(env | egrep '^INPUT_|^GITHUB_|^RUNNER_|^ACTIONS_' | sed -e 's+=.*$++')
+vars=$(echo $vars | sed -e 's+ + -e +g')
+vars=${vars:+"-e $vars"}
+if [ -n "${HOME}" ] ; then
+  vars="${vars} -e HOME"
+fi
+if [ -n "${CI}" ] ; then
+  vars="${vars} -e CI"
 fi
 
-if [ ! -z $INPUT_DOCKER_NETWORK ];
-then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+mounts="-v /var/run/docker.sock:/var/run/docker.sock"
+if [ -n "${RUNNER_TEMP}" ] ; then
+  mounts="${mounts} -v ${RUNNER_TEMP}/_github_home:/github/home"
+  mounts="${mounts} -v ${RUNNER_TEMP}/_github_workflow:/github/workflow"
+  mounts="${mounts} -v ${RUNNER_TEMP}/_runner_file_commands:/github/file_commands"
 fi
 
-exec docker run -v "/var/run/docker.sock":"/var/run/docker.sock" $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
+if [ -n "${RUNNER_WORKSPACE}" -a -n "${GITHUB_REPOSITORY}" ] ; then
+  mounts="${mounts} -v ${RUNNER_WORKSPACE}/${GITHUB_REPOSITORY//*\//}:${GITHUB_WORKSPACE}"
+fi
+
+if [ -n "${RUNNER_TOOL_CACHE}" ] ; then
+  mounts="${mounts} -v ${RUNNER_TOOL_CACHE}:${RUNNER_TOOL_CACHE}"
+fi
+
+echo exec docker run --workdir "${GITHUB_WORKSPACE}" --rm ${vars} ${mounts} "${INPUT_IMAGE}"
